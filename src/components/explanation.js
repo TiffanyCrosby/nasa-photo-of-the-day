69e4b82daa0c2b86ebe3320f0ba641fff2662c3e
Tiffany Crosby <tiffany.crosby65@gmail.com>
2020-02-20 20:23:33 -0500

    Finished styling the image, explanation, and adding a button

--- src/components/explanation.js
+++ src/components/explanation.js
@@ -1,8 +1,33 @@
 import React from "react";
+import styled from 'styled-components';
+
+const Button = styled.button`
+    padding: 6px 10px;
+    margin: 5px;
+    border: none;
+    border-radius: 3px;
+    color: white;
+
+    ${props => (props.type === 'success'/*'success' and hex color is what you change to get something different */ ? `background: #4caf50;` : null)}
+`;
+
+const StyledExplanation = styled.p`
+    width: 80vw;
+    height: 30vh;
+    border: 5px double red;
+    text-align: center;
+    margin: 0 auto;
+    padding: 2%;
+`;
 
 const Explanation = ({ explanation }) => {
 
-    return <p>{explanation}</p>
+    return (
+        <div>
+            <Button type="success">Click Me!!</Button>
+            <StyledExplanation>{explanation}</StyledExplanation>
+        </div>
+    )
 }
 
 export default Explanation;
--- src/components/image.js
+++ src/components/image.js
@@ -1,7 +1,16 @@
 import React from "react";
+import styled from 'styled-components';
+
+const StyledImage = styled.img`
+    width:60vw;
+    height:40vh;
+    margin: 2%;
+    border: none;
+    border-radius: 10px;
+    `;
 
 const Image = ({ image }) => {
-    return <img alt='The Fastest Rotating Galaxy Known' src={image} />
+    return <StyledImage alt='The Fastest Rotating Galaxy Known' src={image} />
 };
 
 export default Image;
